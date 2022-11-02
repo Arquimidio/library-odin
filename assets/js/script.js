e8de82db3d767638b9d4c6878411767f829b14a0
Arquimidio <gabrielstoppadossantos@gmail.com>
2022-11-02 19:46:50 -0300

    Refactor to use localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,12 @@
-const storedBooks = [];
 const bookDisplay = document.querySelector('.booklist');
 const bookForm = document.getElementById('book-form');
 const showForm = document.getElementById('add-book-form-btn');
 const hideForm = document.getElementById('close-form-btn');
+
+/* Starts a new index for localstorage if it doesn't have one */
+if(!localStorage.getItem('index')){
+    localStorage.setItem('index', 0);
+}
 
 /* Creates a book object */
 function Book(title, author, pages, wasRead) {
@@ -12,13 +16,32 @@ function Book(title, author, pages, wasRead) {
     this.wasRead = wasRead? true : false;
 }
 
+/* Gets the current index from the local storage */
+function getCurIndex() {
+    return localStorage.getItem('index');
+}
+
+/* Gets all the books stored in the local storage */
+function getAllBooks() {
+    return Object.entries(localStorage)
+        .filter(entry => entry[0] !== 'index')
+        .map(entry => [entry[0], JSON.parse(entry[1])]);
+}
+
+function changeIndex(newVal) {
+    localStorage.setItem('index', newVal);
+}
+
 /* Adds book to the library array */
 function addBookToLibrary(book) {
-    storedBooks.push(book);
+    const newIndex = Number(getCurIndex()) + 1;
+    localStorage.setItem(newIndex, JSON.stringify(book));
+    changeIndex(newIndex + 1);
+    return newIndex;
 }
 
 /* Creates the necessary elements to display a book with HTML */
-function displaySingleBook({ title, author, pages, wasRead }, id) {
+function displaySingleBook([id, { title, author, pages, wasRead }]) {
     const listItem = document.createElement('li');
     const titleH2 = document.createElement('h2');
     const authorP = document.createElement('p');
@@ -81,8 +104,8 @@ function submitBook(event) {
     const { target: form } = event;
     const formData = new FormData(form);
     const newBook = new Book(...formData.values());
-    addBookToLibrary(newBook);
-    displaySingleBook(newBook, storedBooks.length);
+    const index = addBookToLibrary(newBook);
+    displaySingleBook([index, newBook]);
     form.reset();
     hideBookForm();
 }
@@ -91,7 +114,7 @@ function submitBook(event) {
 function deleteBook(event) {
     const { parentElement: book } = event.target;
     const { id } = book.dataset;
-    storedBooks[id] = null;
+    localStorage.removeItem(id);
     book.remove();
 }
 
@@ -99,12 +122,4 @@ showForm.addEventListener('click', displayBookForm);
 hideForm.addEventListener('click', hideBookForm);
 bookForm.addEventListener('submit', submitBook);
 
-const lotr = new Book(
-    'Lord of the Rings',
-    'Tolkien',
-    '1000',
-    true
-);
-
-addBookToLibrary(lotr);
-displayBooks(storedBooks);
+displayBooks(getAllBooks());
