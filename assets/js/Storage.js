511982902db6950d18d64760aa537234de1867cc
Arquimidio <gabrielstoppadossantos@gmail.com>
2022-11-23 21:07:29 -0300

    Remove empty lines of Storage

--- assets/js/Storage.js
+++ assets/js/Storage.js
@@ -1,56 +1,53 @@
 export default class Storage {
-  constructor() {
-    /* Starts a new index for localstorage if it doesn't have one */
-    if(!localStorage.getItem('index')){
-        localStorage.setItem('index', 0);
+    constructor() {
+        /* Starts a new index for localstorage if it doesn't have one */
+        if (!localStorage.getItem('index')) {
+            localStorage.setItem('index', 0);
+        }
     }
-  }
 
-  /* Gets the current index from the local storage */
-  static getCurIndex() {
-      return localStorage.getItem('index');
-  }
+    /* Gets the current index from the local storage */
+    static getCurIndex() {
+        return localStorage.getItem('index');
+    }
 
-  static nextIndex() {
-    return Number(this.getCurIndex()) + 1;
-   }
+    static nextIndex() {
+        return Number(this.getCurIndex()) + 1;
+    }
 
-  /* Parses received book from localStorage to JSON */
-  static parseBook([prop, val]) {
-      return [prop, JSON.parse(val)];
-  }
+    /* Parses received book from localStorage to JSON */
+    static parseBook([prop, val]) {
+        return [prop, JSON.parse(val)];
+    }
 
-  /* Filter the received book entries (removing the index entry)*/
-  static filterBookEntries([prop]) {
-      return prop !== 'index';
-  }
+    /* Filter the received book entries (removing the index entry)*/
+    static filterBookEntries([prop]) {
+        return prop !== 'index';
+    }
 
-  
     /* Gets a single book from the local storage */
     static getSingleBook(id) {
         return JSON.parse(localStorage.getItem(id));
     }
 
-  /* Gets filtered book entries (removing index entry) from localStorage */
-  static getBookEntries() {
-      const entries = Object.entries(localStorage);
-      return entries.filter(this.filterBookEntries.bind(this));
-  }
+    /* Gets filtered book entries (removing index entry) from localStorage */
+    static getBookEntries() {
+        const entries = Object.entries(localStorage);
+        return entries.filter(this.filterBookEntries.bind(this));
+    }
 
-  /* Gets all books already parsed from the localStorage */
-  static getAllBooks() {
-      const bookEntries = this.getBookEntries();
-      const books = bookEntries.map(this.parseBook.bind(this));
-      return books;
-  }
+    /* Gets all books already parsed from the localStorage */
+    static getAllBooks() {
+        const bookEntries = this.getBookEntries();
+        const books = bookEntries.map(this.parseBook.bind(this));
+        return books;
+    }
 
-  
-
-  /* Adds book to the localStorage */
-  static setBook(id, book) {
-      localStorage.setItem(id, JSON.stringify(book));
-      return book;
-  }
+    /* Adds book to the localStorage */
+    static setBook(id, book) {
+        localStorage.setItem(id, JSON.stringify(book));
+        return book;
+    }
 
     /* Changes localStorage index to an updated value */
     static updateIndex(index) {
